53831871d98bc5608d9648fa196d0d0f11d2b3ad
YmClash <momo.toure27@gmail.com>
2023-07-22 15:36:37 +0200

    add feature and price request on Mexc through the  Mexc-API  SDK  to the function get_balance
     -add label and description
     -test

--- mogwai.py
+++ mogwai.py
@@ -8,6 +8,7 @@ import requests
 
 
 
+
 load_dotenv()
 key = os.getenv("COINMARKKETCAP_API_KEY")
 mexc_client = Spot(api_key=os.getenv('MEXC_ACCESS_KEY'),api_secret=os.getenv('MEXC_SECRET_KEY'))
@@ -22,11 +23,17 @@ rpc_url = SubstrateInterface(url="wss://rpc-parachain.bajun.network")
 ymc_address = ['5G6s3tvwCbQ7MZWYmUrZkG8iYxYwAG8usqM57yJF8bU8Dbvr']
 
 
+
+
+
+
+
 def salut(name) :
     return f"Salut !!! {name}"
 
 
 def get_balance(address) :
+
     url = "https://www.mexc.com/open/api/v2/market/ticker?symbol=BAJU_USDT"
     reponse = requests.get(url)
     data = reponse.json()
@@ -36,18 +43,25 @@ def get_balance(address) :
     balance = Decimal(result.value['data']['free']) / Decimal(10 ** 12)
     balance_format = float(format(balance, '.2f'))
     print(type(result.value['data']['free']))
+    print(address)
     balance_usd = prix_baju * balance_format
     return f"Votre Balance est de : {balance_format} Bajun  ~ {balance_usd:.2f}$ USD"
 
 
 print(rpc_url.get_chain_head())
 
+description2 = 'h1 style="font-size:50px;text-align:center;"> AJUNA Balanc Checker</h1>'
+description = "AJUNA Balance Checker by YmC ^^"
+desk = "Ajuna Balance checker 0.1 "
+
+
 with gradio.Blocks(title="Ajuna Wallet Checker..by YmC") as app :
+    title = gradio.Label(description)
     adresse = gradio.Textbox(label="enter your address")
     nom = gradio.Textbox(label="enter your name")
     balance_output = gradio.Textbox(label="Balance")
     run_button = gradio.Button("RUN")
-    salut_button = gradio.Button("Salu")
+    salut_button = gradio.Button("Say Holla To YmC ^^ ")
     run_button.click(fn=get_balance, inputs=adresse, outputs=balance_output)
     salut_button.click(fn=salut, inputs=nom, outputs=balance_output)
 

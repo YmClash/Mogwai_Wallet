396c7b47099617f837f2e904caa2f840d425f695
YmClash <momo.toure27@gmail.com>
2023-07-23 17:22:11 +0200

    add feature and price request on Mexc through the  Mexc-API  SDK  to the function get_balance
     -add label and description
     -test
     -test deployed for 72H

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import mogwai
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 
 
 # print("Bienvenue dans  le Mogwai Wallet Checker")

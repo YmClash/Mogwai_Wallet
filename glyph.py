6a56b7f7cdbf6bb0db94b9141e45d321afae3aeb
YmClash <momo.toure27@gmail.com>
2023-09-10 23:23:18 +0200

    write code to the Glyph Algo

--- glyph.py
+++ glyph.py
@@ -100,58 +100,118 @@ def gen_glyph(seed):
 
 
 
+def draw(art):
 
-    return hashlib.sha3_256(data).digest()
+    fig, ax = plt.subplots(dpi=300,figsize=(5,5))
+
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    draw_string = art.replace("\n", "")
+    draw_string = [draw_string[i:i+64] for i in range(0, len(draw_string), 64)]
+
+    ax.set_xlim(-1, 65)
+    ax.set_ylim(65, -1)
+
+    c = "k"
+    lw = 0.9
+
+    for x, chars in enumerate(draw_string):
+        for y, char in enumerate(chars):
+            if char == ".":
+                continue
+            elif char == "0":
+                circle  = Circle((x+0.5, y+0.5), radius=0.5, color=c, fill=False)
+                ax.add_patch(circle)
+            elif char == "_":
+                ax.plot([x, x+1], [y+0.5, y+0.5], color=c, lw=lw)
+            elif char == "|":
+                ax.plot([x+0.5, x+0.5], [y, y+1], color=c, lw=lw)
+            elif char == "X":
+                ax.plot([x, x+1], [y, y+1], color=c, lw=lw)
+                ax.plot([x+1, x], [y, y+1], color=c, lw=lw)
+            elif char == "/":
+                ax.plot([x, x+1], [y+1, y], color=c, lw=lw) #revoir ici
+            elif char == "\\":
+                ax.plot([x, x+1], [y, y+1], color=c, lw=lw)
+            elif char == "#":
+                rect = Rectangle((x, y), width=1, height=1, color=c)
+                ax.add_patch(rect)
+            elif char == "+":
+                ax.plot([x+0.5, x+0.5], [y, y+1], color=c, lw=lw)
+                ax.plot([x, x+1], [y+0.5, y+0.5], color=c, lw=lw)
+    return fig
 
 
-def draw(keccak256: bytes):
-    a = int.from_bytes(keccak256(id_to_seed[id].encode()), "big")
-    output = bytearray(USIZE * (USIZE + 3) + 30)
-    c = 0
-    for byte_ in prefix :
-        output[c] = byte_
-        c += 1
 
-    x, y, v, value = 0, 0, 0, 0
+def mint(seed,out_path):
+    p = Path(out_path)
+    art = gen_glyph(seed)
 
-    mod = (a % 11) + 5
+    draw_string = art.replace("\n","")
+    if len(set(draw_string)) == 1:
+        raise ValueError("Glyph may be Dust ")
 
-    # symbols = bytes([0x2E, 0x58, 0x2F, 0x5C, 0x2E, 0x2B, 0x2D, 0x7C, 0x2E, 0x2F,
-    #                  0x5C, 0x2E, 0x2E, 0x5C, 0x7C, 0x2D, 0x2F, 0x4F, 0x7C, 0x2D,
-    #                  0x2E, 0x5C, 0x5C, 0x2E, 0x23, 0x7C, 0x2D, 0x2B, 0x4F, 0x4F,
-    #                  0x2E, 0x23, 0x2E, 0x2E, 0x2E, 0x23, 0x4F, 0x2E, 0x2E
-    #                  ])[id_to_symbol[id]]
+    fig = draw(draw_string)
 
-    if id_to_symbol[id] == 0 :
-        raise ValueError("Symbol not found")
-    elif id_to_symbol[id] == 1 :
-        symbols = b".X/\\."
-    elif id_to_symbol[id] == 2 :
-        symbols = b".+-|."
+    fig.savefig(p/f"Mogwai_{seed}.png", pad_inches=1)
 
-    for i in range(SIZE) :
-        y = (2 * (i + HALF_SIZE) + 1)
-        if a % 3 == 1 :
-            y = -y
-        elif a % 3 == 2 :
-            y = abs(y)
-        y = y * a
-        for j in range(SIZE) :
-            x = (2 * (j - HALF_SIZE) + 1)
-            if a % 2 == 1 :
-                x = abs(x)
-            x = x * a
-            v = (x * y // ONE) % mod
-            if v < 5 :
-                value = (symbols[v])
-            else :
-                value = ord(".")
-            output[c] = value
-            c += 1
-            output[c :c + 3] = b"%0A"
-            c += 3
+    plt.close("all")
 
-            return output.decode(encoding='utf-8', errors='strict')
+
+
+
+    # a = int.from_bytes(keccak256(id_to_seed[id].encode()), "big")
+    # output = bytearray(USIZE * (USIZE + 3) + 30)
+    # c = 0
+    # for byte_ in prefix :
+    #     output[c] = byte_
+    #     c += 1
+    #
+    # x, y, v, value = 0, 0, 0, 0
+    #
+    # mod = (a % 11) + 5
+    #
+    # # symbols = bytes([0x2E, 0x58, 0x2F, 0x5C, 0x2E, 0x2B, 0x2D, 0x7C, 0x2E, 0x2F,
+    # #                  0x5C, 0x2E, 0x2E, 0x5C, 0x7C, 0x2D, 0x2F, 0x4F, 0x7C, 0x2D,
+    # #                  0x2E, 0x5C, 0x5C, 0x2E, 0x23, 0x7C, 0x2D, 0x2B, 0x4F, 0x4F,
+    # #                  0x2E, 0x23, 0x2E, 0x2E, 0x2E, 0x23, 0x4F, 0x2E, 0x2E
+    # #                  ])[id_to_symbol[id]]
+    #
+    # if id_to_symbol[id] == 0 :
+    #     raise ValueError("Symbol not found")
+    # elif id_to_symbol[id] == 1 :
+    #     symbols = b".X/\\."
+    # elif id_to_symbol[id] == 2 :
+    #     symbols = b".+-|."
+    #
+    # for i in range(SIZE) :
+    #     y = (2 * (i + HALF_SIZE) + 1)
+    #     if a % 3 == 1 :
+    #         y = -y
+    #     elif a % 3 == 2 :
+    #         y = abs(y)
+    #     y = y * a
+    #     for j in range(SIZE) :
+    #         x = (2 * (j - HALF_SIZE) + 1)
+    #         if a % 2 == 1 :
+    #             x = abs(x)
+    #         x = x * a
+    #         v = (x * y // ONE) % mod
+    #         if v < 5 :
+    #             value = (symbols[v])
+    #         else :
+    #             value = ord(".")
+    #         output[c] = value
+    #         c += 1
+    #         output[c :c + 3] = b"%0A"
+    #         c += 3
+    #
+    #         return output.decode(encoding='utf-8', errors='strict')
 
 
 id_to_seed[1] = "YmC"

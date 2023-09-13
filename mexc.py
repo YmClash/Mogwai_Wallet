ae009986dbcf3a57a19cc696717eddfe478e0546
YmClash <momo.toure27@gmail.com>
2023-09-13 21:26:22 +0200

    delete useless code and add more sec for the private key

--- mexc.py
+++
@@ -1,9 +0,0 @@
-import gradio as gr
-
-def greet(name):
-  return "Bonjour " + name + " !"
-
-description = '<h1 style="font-size:70px;">AJUNA WALLET CHECKER </h1>'
-
-iface = gr.Interface(fn=greet, inputs="text", outputs="text", description=description)
-iface.launch()

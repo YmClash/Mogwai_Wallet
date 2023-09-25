20029950d7ac0a45c8ca95cdb963cacf17c138b4
YmClash <momo.toure27@gmail.com>
2023-09-26 01:26:30 +0200

    update

---
+++ test_room_5.py
@@ -0,0 +1,15 @@
+import customtkinter
+
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+app = customtkinter.CTk()
+app.geometry("500x500")
+app.title("MogLogin")
+
+
+
+
+app.mainloop()
+

8b2bbbef9edfd90bb032b801ed594c3e59ad1db2
YmClash <momo.toure27@gmail.com>
2023-09-30 04:56:54 +0200

    update

--- test_room_5.py
+++ test_room_5.py
@@ -1,4 +1,5 @@
 import customtkinter
+import sys
 
 
 customtkinter.set_appearance_mode("dark")
@@ -8,6 +9,26 @@ app = customtkinter.CTk()
 app.geometry("500x500")
 app.title("MogLogin")
 
+def run():
+    print("run")
+
+def exit():
+    print("exit")
+    sys.exit()
+
+frame = cous
+
+
+
+
+# run_batton= customtkinter.CTkButton(app,text="RUN",command =run())
+# run_batton.place(relx=0.5,rely=0.5,anchor=tkinter.CENTER)
+#
+# exit_button = customtkinter.CTkButton(app,text="EXIT",command = exit())
+#
+#
+#
+# run_batton.place(relx=0.5,rely=0.5,anchor=tkinter.CENTER)
 
 
 
